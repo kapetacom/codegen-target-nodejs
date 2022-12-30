fce31961a31c1edc5a1be05b2dc05df9ff4cff9f
Henrik Hofmeister <henrik@newdawn.dk>
2022-12-30 17:12:44 +0100

    Added support for versions (#3)
    * Added support for versions
    
    * Fix build script
    
    * Publish to internal NPM

--- test/resources/examples/todo/scripts/docker-build.sh
+++ test/resources/examples/todo/scripts/docker-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Building docker image";
-docker build . -t todo || exit 1
+docker build . -t blockware/todo || exit 1
 
-echo "Done. Image ready: todo";
+echo "Done. Image ready: blockware/todo";
