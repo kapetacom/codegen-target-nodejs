56178e9e1a18e1cf5deda7335da1f9374ac88747
Henrik Hofmeister <henrik@newdawn.dk>
2023-06-22 17:45:17 +0200

    fix: Do not commit prisma clients
    fix: Add gitignore file and build step to ensure client is available
    
    feat: Auto-run migration when starting

--- test/resources/examples/todo/src/data/todo/index.d.ts
+++
@@ -1,9 +0,0 @@
-// Placeholder file - will be overwritten by Prisma when you run npm migrate:dev
-
-export declare namespace Prisma {
-    declare type PrismaClientOptions = any;
-}
-
-export declare class PrismaClient {
-    constructor(opts: Prisma.PrismaClientOptions);
-}
--- test/resources/examples/users/src/data/users/index.js
+++
@@ -1,8 +0,0 @@
-// Placeholder file - will be overwritten by Prisma when you run npm migrate:dev
-
-module.exports = {
-    PrismaClient: function () {},
-    Prisma: {
-        PrismaClientOptions: {},
-    },
-};
