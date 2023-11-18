665761817866d3162b128f36d97f842e31ba08d6
Henrik Hofmeister <henrik@newdawn.dk>
2023-11-18 11:03:32 +0100

    feat: Add supported resource kinds (#38)

--- src/web/index.ts
+++ src/web/index.ts
@@ -13,6 +13,13 @@ const targetConfig: ILanguageTargetProvider = {
     version: packageJson.version,
     title: kapetaDefinition.metadata.title,
     blockKinds: ['kapeta/block-type-service'],
+    resourceKinds: [
+        'kapeta/resource-type-mongodb',
+        'kapeta/resource-type-postgresql',
+        'kapeta/resource-type-rest-api',
+        'kapeta/resource-type-rest-client',
+        'kapeta/resource-type-smtp-client',
+    ],
     definition: kapetaDefinition,
 };
 
