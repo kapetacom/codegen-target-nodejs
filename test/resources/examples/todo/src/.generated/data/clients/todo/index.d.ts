b68b367b6eb78805ea926e7cb2bb2dc99fe4a7e7
Henrik Hofmeister <henrik@newdawn.dk>
2023-12-12 13:27:20 +0100

    add prisma placeholders

---
+++ test/resources/examples/todo/src/.generated/data/clients/todo/index.d.ts
@@ -0,0 +1,12 @@
+// Placeholder file - will be overwritten by Prisma when you run npm migrate:dev
+
+export declare namespace Prisma {
+    declare type PrismaClientOptions = any;
+}
+
+export declare class PrismaClient {
+    constructor(opts: Prisma.PrismaClientOptions);
+
+    $connect();
+    $disconnect();
+}
---
+++ test/resources/examples/users/src/.generated/data/clients/users/index.js
@@ -0,0 +1,8 @@
+// Placeholder file - will be overwritten by Prisma when you run npm migrate:dev
+
+module.exports = {
+    PrismaClient: function () {},
+    Prisma: {
+        PrismaClientOptions: {},
+    },
+};
