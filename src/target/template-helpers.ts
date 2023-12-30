cddf72a644ad5deed5ac9cbc2944f85238c715f9
Henrik Hofmeister <henrik@newdawn.dk>
2023-12-30 17:40:34 +0100

    licenses

--- src/target/template-helpers.ts
+++ src/target/template-helpers.ts
@@ -1,3 +1,7 @@
+/**
+ * Copyright 2023 Kapeta Inc.
+ * SPDX-License-Identifier: MIT
+ */
 import Handlebars = require('handlebars');
 import { Template, TypeLike } from '@kapeta/codegen-target';
 import { BlockDefinitionSpec, Resource } from '@kapeta/schemas';
