861607d57620046cb2e900b1c35a4fc28975dda0
Henrik Hofmeister <henrik@newdawn.dk>
2024-01-09 12:10:15 +0100

    feat: Simplify REST route generation (#61)
    Making types less scary and boilerplate more easily understandable

--- test/resources/examples/todo/src/.generated/rest/TasksRoutes.ts
+++ test/resources/examples/todo/src/.generated/rest/TasksRoutes.ts
@@ -1,8 +1,44 @@
 //
 // GENERATED SOURCE - DO NOT EDIT
 //
-import type { RequestHandler } from 'express';
+import type { Request, Response } from 'express';
 import { Task } from 'generated:entities/Task';
+
+export type AddTaskRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    { userId: string; id: string },
+    void,
+    Task,
+    void,
+    Locals
+>;
+export type AddTaskResponse<Locals extends Record<string, any> = Record<string, any>> = Response<void, Locals>;
+
+export type MarkAsDoneRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    { id: string },
+    void,
+    void,
+    void,
+    Locals
+>;
+export type MarkAsDoneResponse<Locals extends Record<string, any> = Record<string, any>> = Response<void, Locals>;
+
+export type RemoveTaskRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    { id: string },
+    void,
+    void,
+    void,
+    Locals
+>;
+export type RemoveTaskResponse<Locals extends Record<string, any> = Record<string, any>> = Response<void, Locals>;
+
+export type GetTaskRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    { id: string },
+    Task,
+    void,
+    void,
+    Locals
+>;
+export type GetTaskResponse<Locals extends Record<string, any> = Record<string, any>> = Response<Task, Locals>;
 
 /**
  * Defines the methods and routes for the tasks API
@@ -12,23 +48,23 @@ export interface TasksRoutes<Locals extends Record<string, any> = Record<string,
      * Add task for user
      * HTTP: POST /tasks/:userId/:id
      */
-    addTask: RequestHandler<{ userId: string; id: string }, void, Task, void, Locals>;
+    addTask(req: AddTaskRequest<Locals>, res: AddTaskResponse<Locals>): void | Promise<void>;
 
     /**
      * Mark task as done
      * HTTP: POST /tasks/:id/done
      */
-    markAsDone: RequestHandler<{ id: string }, void, void, void, Locals>;
+    markAsDone(req: MarkAsDoneRequest<Locals>, res: MarkAsDoneResponse<Locals>): void | Promise<void>;
 
     /**
      * Delete task
      * HTTP: DELETE /tasks/:id
      */
-    removeTask: RequestHandler<{ id: string }, void, void, void, Locals>;
+    removeTask(req: RemoveTaskRequest<Locals>, res: RemoveTaskResponse<Locals>): void | Promise<void>;
 
     /**
      *
      * HTTP: GET /tasks/:id
      */
-    getTask: RequestHandler<{ id: string }, Task, void, void, Locals>;
+    getTask(req: GetTaskRequest<Locals>, res: GetTaskResponse<Locals>): void | Promise<void>;
 }
--- test/resources/examples/users/src/.generated/rest/UsersRoutes.ts
+++ test/resources/examples/users/src/.generated/rest/UsersRoutes.ts
@@ -1,8 +1,35 @@
 //
 // GENERATED SOURCE - DO NOT EDIT
 //
-import type { RequestHandler } from 'express';
+import type { Request, Response } from 'express';
 import { User } from 'generated:entities/User';
+
+export type CreateUserRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    { id: string },
+    User,
+    Map<string, string>,
+    { user: User; tags?: Set<string> },
+    Locals
+>;
+export type CreateUserResponse<Locals extends Record<string, any> = Record<string, any>> = Response<User, Locals>;
+
+export type GetUserRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    { id: string },
+    User,
+    void,
+    void,
+    Locals
+>;
+export type GetUserResponse<Locals extends Record<string, any> = Record<string, any>> = Response<User, Locals>;
+
+export type DeleteUserRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    { id: string },
+    void,
+    void,
+    void,
+    Locals
+>;
+export type DeleteUserResponse<Locals extends Record<string, any> = Record<string, any>> = Response<void, Locals>;
 
 /**
  * Defines the methods and routes for the users API
@@ -12,17 +39,17 @@ export interface UsersRoutes<Locals extends Record<string, any> = Record<string,
      * Create user
      * HTTP: POST /users/:id
      */
-    createUser: RequestHandler<{ id: string }, User, Map<string, string>, { user: User; tags?: Set<string> }, Locals>;
+    createUser(req: CreateUserRequest<Locals>, res: CreateUserResponse<Locals>): void | Promise<void>;
 
     /**
      * Get user by id
      * HTTP: GET /users/:id
      */
-    getUser: RequestHandler<{ id: string }, User, void, void, Locals>;
+    getUser(req: GetUserRequest<Locals>, res: GetUserResponse<Locals>): void | Promise<void>;
 
     /**
      * Delete user by id
      * HTTP: DELETE /users/:id
      */
-    deleteUser: RequestHandler<{ id: string }, void, void, void, Locals>;
+    deleteUser(req: DeleteUserRequest<Locals>, res: DeleteUserResponse<Locals>): void | Promise<void>;
 }
